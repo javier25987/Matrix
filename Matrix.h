21ac76ae1c413649443220bc4a72f4d0eab0b929
javier25987 <fernando71153049@gmail.com>
2024-11-15 13:26:14 +0300

    Metod get_colum

--- Matrix.h
+++ Matrix.h
@@ -1,88 +1,22 @@
 #ifndef Matrix_H
 #define Matrix_H
 
-
-/*
-#include<vector>
-#include <initializer_list>
-
-template<class T>
-class Matrix
-{
-	std::vector<std::vector<T>> _MX;
-	std::pair<int, int> _size;
-
-public:
-	Matrix(std::vector<std::vector<T>>& a);
-
-	void change(std::pair<int, int> xy, T a)
-	{
-		_MX[xy.first()][xy.second()] = a;
-	};
-
-	void transpon() 
-	{
-		std::vector<std::vector<T>> a;
-		for (size_t i = 0; i < _size.first(); i++) {
-			for (size_t j = 0; j < _size.second(); j++) {
-				a[i][j] = _MX[j][i];
-			}
-		}
-		this->_MX = a;
-		delete a;
-	};
-
-	std::vector<T> get_line(size_t i) const
-	{
-		return _MX[i];
-	}
-
-	vector<vector<T>> get_lines(pair xy) 
-	{
-		vector<vector<T>> a;
-		for (size_t i = xy.first(); i < xy.second(); i++) {
-			a += _MX[i]
-		}
-		return a;
-	};
-	Matrix sum(Matrix b) 
-	{
-		vector<vector<T>> a;
-		for (size_t i = 0; i < _size.first(); i++) {
-			for (size_t j = 0; j < _size.second(); j++) {
-				a[i][j] = _MX[i][j] + b[i][j];
-			}
-		}
-		return a;
-	};
-	Matrix prod_const(T a) 
-	{
-		vector<vector<T>> b;
-		for (size_t i = 0; i < _size.first(); i++) {
-			for (size_t j = 0; j < _size.second(); j++) {
-				b[i][j] = _MX[i][j] * a;
-			}
-		}
-		return b;
-	}
-
-	Matrix prod(Matrix b) {
-		vector<vector<T>> a;
-		T sum;
-		Matrix k = b.transpon();
-		for (size_t i = 0; i < _size.first(); i++) {							  work in progress
-			for (size_t j = 0; j < _size.second(); j++) {
-				sum += _MX[i][j] * k[i][j];
-			}
-		}
-		return a;
-	}
-};
-*/
-
 #include <vector>
 #include <initializer_list>
 #include <iostream>
+
+
+template <class T>
+void printV(std::vector<T> vec)
+{
+	std::cout << "( ";
+	for (const T x : vec)
+	{
+		std::cout << x << " ";
+	}
+	std::cout << ")" << std::endl;
+}
+
 
 template<typename T>
 class Matrix {
@@ -110,6 +44,25 @@ public:
 		else 
 		{
 			throw std::invalid_argument("row out range");
+		}
+	}
+
+
+	std::vector<T> get_colum(int row)
+	{
+		if (0 <= row && row < _MX[0].size())
+		{
+			std::vector<T> temporal_v;
+
+			for (std::vector<T>& elem : _MX)
+			{
+				temporal_v.push_back(elem[row]);
+			}
+			return temporal_v;
+		}
+		else
+		{
+			throw std::invalid_argument("colum out range");
 		}
 	}
 
