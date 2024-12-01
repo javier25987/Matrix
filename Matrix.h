3e2986107775a4428394eb69f6d56e9ebc62d779
javier25987 <fernando71153049@gmail.com>
2024-12-01 20:13:37 +0300

    frish rang function'

--- Matrix.h
+++ Matrix.h
@@ -4,6 +4,7 @@
 #include <vector>
 #include <initializer_list>
 #include <iostream>
+#include <cmath>
 
 
 template <class T>
@@ -15,6 +16,18 @@ void printV(std::vector<T> vec)
 		std::cout << x << " ";
 	}
 	std::cout << "}" << std::endl;
+}
+
+// ================= javier's functions
+
+template <class T>
+bool all_is_zero(std::vector<T> vec)
+{
+	for (T x : vec)
+	{
+		if (std::abs(x) > 1e-9) { return false; }
+	}
+	return true;
 }
 
 
@@ -100,6 +113,101 @@ public:
 			std::cout << "}" << "\n";
 		}
 		std::cout << "}\n";
+	}
+
+	// ==========  here is javier's work
+
+	int rang() const
+	{
+		int rows = _MX.size();
+		int cols = _MX[0].size();
+
+		std::vector<std::vector<T>> matrix;
+		
+		if (rows > cols)
+		{
+			std::vector<T> temporal_v;
+
+			for (size_t i = 0; i < cols; i++)
+			{
+				for (std::vector<T> x : _MX)
+				{
+					temporal_v.push_back(x[i]);
+				}
+
+				matrix.push_back(temporal_v);
+				temporal_v.clear();
+			}
+			std::swap(rows, cols);
+		}
+		else
+		{
+			matrix = _MX;
+		}
+
+		int v_ind = 0, h_ind = 0;
+		bool colum_is_cero = false;
+		double factor, pivot;
+
+		while (v_ind < rows && h_ind < cols)
+		{
+
+			std::cout << "vertical: " << v_ind << " ; horizontal: " << h_ind << std::endl;
+
+			for (std::vector<T> x : matrix)
+			{
+				printV(x);
+			}
+
+			if (std::abs(matrix[v_ind][h_ind]) < 1e-9)
+			{
+				if (all_is_zero(matrix[v_ind]))
+				{
+					std::swap(matrix[v_ind], matrix[rows - 1]);
+					rows--;
+					continue;
+				}
+				else
+				{
+					colum_is_cero = true;
+					for (int i = v_ind + 1; i < rows; i++)
+					{
+						if (std::abs(matrix[i][h_ind]) > 1e-9)
+						{
+							std::swap(matrix[v_ind], matrix[i]);
+							colum_is_cero = false;
+							break;
+						}
+					}
+					if (colum_is_cero)
+					{
+						h_ind++;
+						colum_is_cero = false;
+						continue;
+					}
+				}
+			}
+			
+			pivot = matrix[v_ind][h_ind];
+
+			for (int i = v_ind + 1; i < rows; i++) 
+			{
+				factor = matrix[i][h_ind] / pivot;
+
+				for (int j = h_ind; j < cols; j++) 
+				{
+					std::cout << "lv: " << i << " ; lh: " << j << " ; fac: " << factor << "\n";
+					matrix[i][j] -= factor * matrix[v_ind][j];
+				}
+			}
+
+			v_ind++;
+			h_ind++;
+
+			std::cout << "--------------------------------\n";
+		}
+
+		return rows;
 	}
 };
 
--- main.cpp
+++ main.cpp
@@ -1,15 +1,18 @@
 #include <vector>
 #include <initializer_list>
 #include <iostream>
-#include <vector>
 #include "Matrix.h"
 
 int main() {
-    Matrix<int> matrix = {
-        {1, 2, 3},
-        {4, 5, 6}
+    Matrix<int> matrix = 
+    {
+        {  1,  0, -1,  1 },
+        {  2, -2,  1,  7 },
+        { -1, -1,  3,  2 },
+        {  0, -1,  1,  2 }
     };
 
+    /*
     matrix.printM();
 
     printV(matrix.get_row(0));
@@ -20,5 +23,10 @@ int main() {
         matrix.transpose();
         matrix.printM();
     }
+    */
+
+    std::cout << "Rang = " << matrix.rang() << "\n";
+
+    system("pause");
     return 0;
 }
